8af40bf6e7ceb772e7eeaf99beaec7d9dce42503
Konrad Król <k.krol@oberthur.com>
2015-11-01 19:01:35 +0100

    Added support of specific port for standalone redis instance

--- slave/start-slave.sh
+++ slave/start-slave.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [ -z "$REDIS_MASTER_PORT_6379_TCP_ADDR" ]; then
-    echo "REDIS_MASTER_PORT_6379_TCP_ADDR not defined. Did you run with -link?";
+    echo "REDIS_MASTER_PORT_6379_TCP_ADDR not defined. Please run with --link or specify environment variables manually.";
     exit 7;
 fi
 
+if [ -z "$PORT" ]; then
+    echo "PORT environment variable is not defined. Will use default port 6379";
+    PORT='6379'
+fi
 
-exec /usr/local/bin/redis-server --slaveof $REDIS_MASTER_PORT_6379_TCP_ADDR $REDIS_MASTER_PORT_6379_TCP_PORT $*
+exec /usr/local/bin/redis-server --slaveof $REDIS_MASTER_PORT_6379_TCP_ADDR $REDIS_MASTER_PORT_6379_TCP_PORT --port $PORT $*
