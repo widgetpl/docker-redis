e9a816dbf1ddd6256815ba276032eca5180b3ba1
Kacper Piechota <k.piechota@oberthur.com>
2016-02-23 13:34:20 +0100

    Update redis to latest version 3.0.7, remove starting scripts, set default CMD to do image more flexible

--- cluster/start-cluster-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$PORT" ]; then
-    echo "PORT environment variable is not defined. Will use default port 6379";
-    PORT='6379'
-fi
-
-exec /usr/local/bin/redis-server /etc/redis.conf --port $PORT
