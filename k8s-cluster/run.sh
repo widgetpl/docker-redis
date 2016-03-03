b447460bc3b9eaae17c71aed59d3bc14289c7b3b
Michal Dziedziela <m.dziedziela@oberthur.com>
2016-03-03 09:55:22 +0100

    Removed CAN_START to be able to run redis in default way ( without CLUSTER_CONFIG and REDIS_CONFIG )

--- k8s-cluster/run.sh
+++ k8s-cluster/run.sh
@@ -10,12 +10,9 @@ LEVEL_INFO="${CL_BLU}INFO${CL_RST} "
 LEVEL_WARN="${CL_RED}WARN${CL_RST} "
 LEVEL_ERROR="${CL_RED}ERROR${CL_RST}"
 
-CAN_START=true
-
 if [ -z "${CLUSTER_CONFIG}" ]; then
 
   echo "${LEVEL_ERROR} CLUSTER_CONFIG env variable not specified."
-  CAN_START=false
 
 else
 
@@ -30,39 +27,30 @@ else
     eval echo $line
     eval echo $line >> nodes.conf
   done < nodes.tmp
-
-  if [ -z "${REDIS_CONFIG}" ]; then
-
-    echo "${LEVEL_INFO} CLUSTER_CONFIG env variable not specified, no more options will be applied"
-
-  else
-
-    printf "Applying redis configuration from REDIS_CONFIG:\n"
-
-    while read -r line
-    do
-
-      printf "$line\n"
-      printf "$line\n" >> /etc/redis.conf
-
-    done <<< "$REDIS_CONFIG"
-
-  fi
-
 fi
 
-if [ "${CAN_START}" = true ]; then
+if [ -z "${REDIS_CONFIG}" ]; then
 
-  if [ "$1" = 'redis-server' ]; then
-    chown -R redis .
-    exec gosu redis "$@"
-  fi
-
-  exec "$@"
+  echo "${LEVEL_INFO} CLUSTER_CONFIG env variable not specified, no more options will be applied or use your own redis.conf file"
 
 else
 
-    echo -e "${LEVEL_ERROR} Some of mandatory environment variables was not specified. Exiting"
-    exit 1
+  printf "Applying redis configuration from REDIS_CONFIG:\n"
+
+  while read -r line
+  do
+
+    printf "$line\n"
+    printf "$line\n" >> /etc/redis.conf
+
+  done <<< "$REDIS_CONFIG"
 
 fi
+
+
+if [ "$1" = 'redis-server' ]; then
+  chown -R redis .
+  exec gosu redis "$@"
+fi
+
+exec "$@"
